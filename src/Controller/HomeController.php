1f1902d4ad962ccc482c72464616b1c63e56c45b
Fizioh <batrom@laposte.net>
2021-05-27 22:06:49 +0200

    pull from main remote branch

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,11 +4,8 @@ use Twig\Environment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-<<<<<<< HEAD
-=======
 use App\Repository\PropertyRepository;
 
->>>>>>> test/Fizioh
 
 
 class HomeController extends AbstractController {
@@ -16,18 +13,12 @@ class HomeController extends AbstractController {
      * @Route("/"), name="home")
      * @return Response
      */
-    
-<<<<<<< HEAD
-    public function index(){
-
-        return $this->render('pages/home.html.twig');
-=======
     public function index(PropertyRepository $repository){
+
 
         $properties = $repository->findLatest();
         return $this->render('pages/home.html.twig', [
             'properties' => $properties
         ]);
->>>>>>> test/Fizioh
     }
 }
