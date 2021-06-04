7298e67f4e434a7b29f2236a91944138a1cb766b
Fizioh <batrom@laposte.net>
2021-06-04 12:25:10 +0200

    auth completed

--- migrations/Version20210531060614.php
+++ migrations/Version20210531060614.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210527094102 extends AbstractMigration
+final class Version20210531060614 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20210527094102 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE property ADD test VARCHAR(255) DEFAULT NULL, CHANGE sold sold TINYINT(1) DEFAULT \'0\' NOT NULL');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE property DROP test, CHANGE sold sold TINYINT(1) NOT NULL');
+        $this->addSql('DROP TABLE user');
     }
 }
